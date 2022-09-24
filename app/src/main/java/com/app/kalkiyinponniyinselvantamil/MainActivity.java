746b923df86f2be22f8cc5101af9a51117332581
SathyaPur <msathyadevi@gmail.com>
2022-09-24 18:26:31 +0530

    Initial commit

---
+++ app/src/main/java/com/app/kalkiyinponniyinselvantamil/MainActivity.java
@@ -0,0 +1,36 @@
+package com.app.kalkiyinponniyinselvantamil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView= findViewById(R.id.Listviewid);
+
+
+        String[] story_name = getResources().getStringArray(R.array.Chapters_Name);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,story_name);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}
